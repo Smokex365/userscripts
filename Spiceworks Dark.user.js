0b205b633ba8b62427c7ab1382152efa1fb0bf1d
Michael Chamberland <smokex365@gmail.com>
2016-03-19 00:42:23 -0500

    Update Spiceworks Dark.user.js

--- Spiceworks Dark.user.js
+++ Spiceworks Dark.user.js
@@ -5,7 +5,8 @@
 // @match			https://*.spiceworks.com/*
 // @grant			GM_addStyle
 // @version         0.1
-// @updateURL		
+// @updateURL		https://github.com/Smokex365/userscripts/raw/master/Spiceworks%20Dark.user.js
+// @supportURL 		https://github.com/Smokex365/userscripts/issues
 // ==/UserScript==
 
 function addGlobalStyle(css) {
@@ -18,13 +19,25 @@ function addGlobalStyle(css) {
     head.appendChild(style);
 }
 
-//Layout
+//ayout
 addGlobalStyle('.base .sui-fluid-container body.sui- .sui-body {background-color: #292D30 !important;}');
-addGlobalStyle('container.sui-fluid-container {background-color: #292D30 !important;}');
+addGlobalStyle('container {background-color: #292D30 !important;}');
 addGlobalStyle('body.sui-* {background-color: #292D30 !important;}');
 addGlobalStyle('.sui-body {background-color: #292D30 !important;}');
+addGlobalStyle('body, body.sui-opt-in {background-color: #292D30 !important;}');
+addGlobalStyle('nav {background-color: #292D30 !important;}');
+addGlobalStyle('.domestic-menus_entry > a {background-color: #292D30 !important;}');
+addGlobalStyle('.sui-opt-in .domestic-menus_submenu {background-color: #292D30 !important;}');
+
 
 //Text
 addGlobalStyle('.nb_sidebar h2 {color: #DC5B23 !important;}');
 addGlobalStyle('.sui-feed-head a, .sui-head a {color: #DC5B23 !important;}');
 addGlobalStyle('.sui-body-text {color: #DC5B23 !important;}');
+addGlobalStyle('h1, h2, h3, h4, h5 {color: #DC5B23 !important;}');
+addGlobalStyle('body {color: #DC5B23 !important;}');
+addGlobalStyle('body.no-body-shadow .sui-body header h1 a {color: #DC5B23 !important;}');
+addGlobalStyle('p {color: #AF5026 !important;}');
+
+
+// addGlobalStyle(' {color: #DC5B23 !important;}');
